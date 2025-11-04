2089115610889029584e31355d543168a8d9ec43
kosterseb <sebkos.code@gmail.com>
2025-11-04 11:07:32 +0100

    setup of the components, before moving on to new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+// Components
+import TicTacToe from './components/TicTacToe.jsx'
+
 function App() {
 
   return (
     <>
-     
+     <TicTacToe />
     </>
   )
 }
---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,8 @@
+export default function TicTacToe() {
+    return (
+        <div>
+        <h1>Tic Tac Toe Game</h1>
+        {/* Game implementation goes here */}
+        </div>
+    )
+    }
